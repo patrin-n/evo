b46abd7746103756497ce042b47647fee17ae097
Parnia <p_naghavi@yahoo.com>
2019-11-25 17:54:25 +0330

    test

---
+++ model.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class EvoMultiPlayerMultiStrategy:
+    def __init__(self, n, m, L):
+        self.n = n
+        self.m = m
+        self.L = L
+
+    def calculate_reward(self, action):
+        action_num = np.zeros(self.m)
+        for i in range(self.m):
+            action_num[i] = sum(action == i)
