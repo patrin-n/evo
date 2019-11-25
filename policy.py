f1c4f7d398f8043dcd0c49160626867d296b88cc
Parnia <p_naghavi@yahoo.com>
2019-11-25 16:47:40 +0330

    test

--- policy.py
+++ policy.py
@@ -1,4 +1,9 @@
 import numpy as np
 
-def softmax(self,q_val):
-  return exp(q_val / temp) / np.sum(exp(q_val / temp))
+
+def softmax(q_val, temp):
+    return np.exp(q_val / temp) / np.sum(np.exp(q_val / temp))
+
+
+def linear(q_val):
+    return q_val / sum(q_val)
