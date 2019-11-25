3f97a8ebc459b76d60fbe4b63f15688dddc12f01
Parnia <p_naghavi@yahoo.com>
2019-11-25 16:47:28 +0330

    test

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import numpy
+import numpy as np
+from policy import *
+
 
 def __main__():
     print("It is a test")
     print("salam")
+    x = np.max([1, 2])
+    print(x)
+    q = np.array([10, 10, 5])
+    print(softmax(q, 50))
+    print(linear(q))
+
+
+__main__()
